21036645c9dbe2c06929669cddb189f270b6c01c
Lily <sh0ck.04@outlook.com>
2025-10-27 20:00:52 -0400

    fix: Update usage information to reflect new command format

--- scheduler.sh
+++ scheduler.sh
@@ -24,11 +24,11 @@ print_help() {
   printf "%s\n" "Usage: $script_name [OPTION...] [COMMAND]
 
 Commands:
-  --update                update the active tasks defined in \$HOME/.config/scheduler
-  --run-workflow <name>   run the <name>.yml workflow in \$HOME/.config/scheduler
-  --list                  list all scheduled tasks
-  --help                  show this help message
-  --version               show the version
+  update                update the active tasks defined in \$HOME/.config/scheduler
+  run-workflow <name>   run the <name>.yml workflow in \$HOME/.config/scheduler
+  list                  list all scheduled tasks
+  help                  show this help message
+  version               show the version
 
 Options:
   -v, --verbose           display extra information while running"
